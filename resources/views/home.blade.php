707020d5b49675c594a8494aa66bc9441127b638
CesarSola <cesarmontero1125@gmail.com>
2024-05-27 16:21:44 -0600

    respondan

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard1</h1>
+@stop
+
+@section('content')
+<<<<<<< HEAD
+    <p>Hola a todos respondan aqui si se pudo.</p>
+    <p>si cesar</p>
+    <p></p>
+    <p></p>
+=======
+    <p>si se nota</p>
+    <p>Hola desde mi repositorio soy cesar 2.</p>
+>>>>>>> 88ec02d3532638ad363cf96a855e8d7a935a2eb6
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script>
+        console.log("Hi, I'm using the Laravel-AdminLTE package!");
+    </script>
+@stop
